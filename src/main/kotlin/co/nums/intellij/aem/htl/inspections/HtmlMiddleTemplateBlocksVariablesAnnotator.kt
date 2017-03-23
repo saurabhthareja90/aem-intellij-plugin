43bfe81e2f9786940f9c14e6671ce912fea2c807
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-03-23 21:50:45 +0100

    #70 simplify code by for one line methods

--- src/main/kotlin/co/nums/intellij/aem/htl/inspections/HtmlMiddleTemplateBlocksVariablesAnnotator.kt
+++ src/main/kotlin/co/nums/intellij/aem/htl/inspections/HtmlMiddleTemplateBlocksVariablesAnnotator.kt
@@ -2,43 +2,28 @@ package co.nums.intellij.aem.htl.inspections
 
 import co.nums.intellij.aem.htl.psi.extensions.XmlElementVisitorExtension
 import co.nums.intellij.aem.htl.psi.extensions.isHtlVariableBlock
-import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.codeInspection.XmlSuppressableInspectionTool
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.xml.XmlAttribute
 import com.intellij.psi.xml.XmlFile
 import com.intellij.psi.xml.XmlTag
-import org.jetbrains.annotations.NotNull
 
 class HtmlMiddleTemplateBlocksVariablesAnnotator : XmlSuppressableInspectionTool() {
 
     private val MESSAGE = "Use data-sly-use variable statements only on root elements"
 
-    override fun getDisplayName(): String {
-        return "Middle template blocks variables annotator"
-    }
+    override fun getDisplayName() = "Middle template blocks variables annotator"
 
-    override fun getShortName(): String {
-        return "HtmlMiddleTemplateBlocksVariablesAnnotator"
-    }
+    override fun getShortName() = "HmlMiddleTemplateBlocksVariablesAnnotator"
 
-    override fun getGroupDisplayName(): String {
-        return "Htl inspections"
-    }
+    override fun getGroupDisplayName() = "Htl inspections"
 
-    override fun getDescriptionContextClass(): Class<out InspectionProfileEntry> {
-        return super.getDescriptionContextClass()
-    }
+    override fun getDescriptionContextClass() = super.getDescriptionContextClass()
 
-    override fun getStaticDescription(): String? {
-        return "Select non root data-sly-use statements variables"
-    }
+    override fun getStaticDescription() = "Select non root data-sly-use statements variables"
 
-
-    override fun isEnabledByDefault(): Boolean {
-        return true
-    }
+    override fun isEnabledByDefault() = true
 
     fun checkAttribute(attribute: XmlAttribute, holder: ProblemsHolder, firstElement: XmlTag?) {
         if (attribute.isHtlVariableBlock() && attribute.name.startsWith("data-sly-use") && (firstElement != null && firstElement != attribute.parent)) {
