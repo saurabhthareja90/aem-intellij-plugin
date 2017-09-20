88d01cf252d97c53cbab098f148cf1d68ce08d70
Karol Lewandowski <karol86@gmail.com>
2017-09-20 17:36:48 +0200

    #87: Too many element types registered. Out of (short) range. (#88)

--- src/main/kotlin/co/nums/intellij/aem/htl/parser/HtlParserDefinition.kt
+++ src/main/kotlin/co/nums/intellij/aem/htl/parser/HtlParserDefinition.kt
@@ -1,8 +1,8 @@
 package co.nums.intellij.aem.htl.parser
 
-import co.nums.intellij.aem.htl.HtlLanguage
 import co.nums.intellij.aem.htl.lexer.HtlLexerAdapter
 import co.nums.intellij.aem.htl.psi.HtlFile
+import co.nums.intellij.aem.htl.psi.HtlFileElementType
 import co.nums.intellij.aem.htl.psi.HtlTypes
 import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
@@ -10,22 +10,24 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.TokenType
-import com.intellij.psi.tree.IFileElementType
 import com.intellij.psi.tree.TokenSet
 
 class HtlParserDefinition : ParserDefinition {
+
+    private val whitespaceTokens = TokenSet.create(TokenType.WHITE_SPACE)
+    private val stringLiteralElements = TokenSet.create(HtlTypes.SINGLE_QUOTED_STRING, HtlTypes.DOUBLE_QUOTED_STRING)
 
     override fun createLexer(project: Project) = HtlLexerAdapter()
 
     override fun createParser(project: Project) = HtlParser()
 
-    override fun getWhitespaceTokens() = TokenSet.create(TokenType.WHITE_SPACE)
+    override fun getWhitespaceTokens() = whitespaceTokens
 
     override fun getCommentTokens(): TokenSet = TokenSet.EMPTY
 
-    override fun getStringLiteralElements() = TokenSet.create(HtlTypes.SINGLE_QUOTED_STRING, HtlTypes.DOUBLE_QUOTED_STRING)
+    override fun getStringLiteralElements() = stringLiteralElements
 
-    override fun getFileNodeType() = IFileElementType(HtlLanguage)
+    override fun getFileNodeType() = HtlFileElementType
 
     override fun createFile(viewProvider: FileViewProvider) = HtlFile(viewProvider)
 
