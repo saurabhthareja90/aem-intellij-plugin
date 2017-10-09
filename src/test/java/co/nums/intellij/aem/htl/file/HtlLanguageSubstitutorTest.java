eabbb31b674e48afd744bf6da51b81e1ce327514
Karol Lewandowski <karol86@gmail.com>
2017-10-09 19:36:07 +0200

    #95: Add JUnit5 (#96)

--- src/test/java/co/nums/intellij/aem/htl/file/HtlLanguageSubstitutorTest.java
+++ src/test/java/co/nums/intellij/aem/htl/file/HtlLanguageSubstitutorTest.java
@@ -1,3 +1,4 @@
+
 package co.nums.intellij.aem.htl.file;
 
 import co.nums.intellij.aem.constants.JcrConstants;
@@ -8,19 +9,17 @@ import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.MockLanguageFileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(DataProviderRunner.class)
-public class HtlLanguageSubstitutorTest {
+class HtlLanguageSubstitutorTest {
 
 	private static final Project ANY_PROJECT = mock(Project.class);
 	private static final FileType NON_HTL_FILE_TYPE = MockLanguageFileType.INSTANCE;
@@ -30,22 +29,24 @@ public class HtlLanguageSubstitutorTest {
 
 	private VirtualFile file;
 
-	@Before
-	public void setUp() {
+	@BeforeEach
+	void setUp() {
 		sut = new HtlLanguageSubstitutor();
 		file = mock(VirtualFile.class);
 	}
 
-	@Test
-	@DataProvider(value = {
+	@ParameterizedTest
+	@ValueSource(strings = {
+			//@formatter:off
 			"jcr_root",
 			"jcr_root/apps",
 			"jcr_root/apps/components",
 			"home/user/aem-project/test-module/jcr_root",
 			"home/user/aem-project/test-module/jcr_root/apps",
 			"home/user/aem-project/test-module/jcr_root/apps/components"
-	}, splitBy = "/")
-	public void shouldSubstituteHtmlFileInJcrRootDirectory(String... path) {
+			//@formatter:on
+	})
+	void shouldSubstituteHtmlFileInJcrRootDirectory(String path) {
 		when(file.getFileType()).thenReturn(HtmlFileType.INSTANCE);
 		prepareFileDirectoryPath(file, path);
 
@@ -55,7 +56,7 @@ public class HtlLanguageSubstitutorTest {
 	}
 
 	@Test
-	public void shouldNotSubstituteNonHtmlFileInJcrRootDirectory() {
+	void shouldNotSubstituteNonHtmlFileInJcrRootDirectory() {
 		when(file.getFileType()).thenReturn(NON_HTL_FILE_TYPE);
 		when(file.getParent()).thenReturn(JCR_ROOT_DIR);
 
@@ -64,9 +65,9 @@ public class HtlLanguageSubstitutorTest {
 		assertThat(language).isNull();
 	}
 
-	@DataProvider(splitBy = "/")
-	public static String[] pathsOutOfJcrRoot() {
+	private static String[] pathsOutOfJcrRoot() {
 		return new String[] {
+				//@formatter:off
 				"not_jcr_root",
 				"jcr_root_not",
 				"_jcr_root",
@@ -82,12 +83,13 @@ public class HtlLanguageSubstitutorTest {
 				"home/user/aem-project/test-module/_jcr_root",
 				"home/user/aem-project/test-module/jcr_root_",
 				"home/user/aem-project/test-module/jcrroot"
+				//@formatter:on
 		};
 	}
 
-	@Test
-	@UseDataProvider("pathsOutOfJcrRoot")
-	public void shouldNotSubstituteHtmlFileOutOfJcrRootDirectory(String... pathOutOfJcrRoot) {
+	@ParameterizedTest
+	@MethodSource("pathsOutOfJcrRoot")
+	void shouldNotSubstituteHtmlFileOutOfJcrRootDirectory(String pathOutOfJcrRoot) {
 		when(file.getFileType()).thenReturn(HtmlFileType.INSTANCE);
 		prepareFileDirectoryPath(file, pathOutOfJcrRoot);
 
@@ -96,9 +98,9 @@ public class HtlLanguageSubstitutorTest {
 		assertThat(language).isNull();
 	}
 
-	@Test
-	@UseDataProvider("pathsOutOfJcrRoot")
-	public void shouldNotSubstituteNonHtmlFileOutOfJcrRootDirectory(String... pathOutOfJcrRoot) {
+	@ParameterizedTest
+	@MethodSource("pathsOutOfJcrRoot")
+	void shouldNotSubstituteNonHtmlFileOutOfJcrRootDirectory(String pathOutOfJcrRoot) {
 		when(file.getFileType()).thenReturn(NON_HTL_FILE_TYPE);
 		prepareFileDirectoryPath(file, pathOutOfJcrRoot);
 
@@ -107,10 +109,11 @@ public class HtlLanguageSubstitutorTest {
 		assertThat(language).isNull();
 	}
 
-	private static void prepareFileDirectoryPath(VirtualFile file, String... path) {
+	private static void prepareFileDirectoryPath(VirtualFile file, String path) {
+		String[] pathParts = path.split("/");
 		VirtualFile currentFile = file;
-		for (int i = path.length - 1; i >= 0; i--) {
-			VirtualFile parentDirectory = directoryWithName(path[i]);
+		for (int i = pathParts.length - 1; i >= 0; i--) {
+			VirtualFile parentDirectory = directoryWithName(pathParts[i]);
 			when(currentFile.getParent()).thenReturn(parentDirectory);
 			currentFile = parentDirectory;
 		}
